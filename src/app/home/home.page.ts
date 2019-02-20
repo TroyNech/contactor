00012ab6580c8e86d1816f10694221e0d57ab49c
TroyNech <troynechanicky@gmail.com>
2019-02-20 01:42:18 -0500

    basic qr generate and scan working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedData: Object;
+  public myAngularxQrCode: string;
 
+  constructor(private barcodeScanner: BarcodeScanner) { }
+
+  createCode() {
+    let data = { "email": "nech5860@mylaurier.ca", "id": 150405860 };
+    this.myAngularxQrCode = JSON.stringify(data);
+  }
+
+  scanCode() {
+    console.log("scanning...");
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.scannedData = JSON.stringify(JSON.parse(barcodeData.text));
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 }
